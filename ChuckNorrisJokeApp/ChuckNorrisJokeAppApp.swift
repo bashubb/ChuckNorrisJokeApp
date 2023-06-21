4a53f32625f32a321f6fa11f33c930e5f4815d5b
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-06-21 22:08:12 +0200

    Initial Commit

---
+++ ChuckNorrisJokeApp/ChuckNorrisJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokeAppApp.swift
+//  ChuckNorrisJokeApp
+//
+//  Created by HubertMac on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
